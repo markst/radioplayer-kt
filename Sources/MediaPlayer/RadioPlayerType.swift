12ad691e238b6248ebf11ecae8aedb28cc0e9751
Mark Turner <turner.mark@abc.net.au>
2024-06-17 19:35:22 +0930

    Initial `RadioPlayerType` protocol.

---
+++ Sources/MediaPlayer/RadioPlayerType.swift
@@ -0,0 +1,29 @@
+import Combine
+import Foundation
+import AVFoundation
+
+public protocol RadioPlayerType {
+    var state: AnyPublisher<RadioPlayerState, Never> { get }
+    var playbackState: AnyPublisher<RadioPlayerPlaybackState, Never> { get }
+    var playProgress: AnyPublisher<Progress, Never> { get }
+    var isPlaying: AnyPublisher<Bool, Never> { get }
+    
+    func play(url: URL, at position: CMTime?)
+    func play()
+    func pause()
+    func stop()
+    func togglePlaying()
+    
+    func seek(delta: Double)
+    func seek(to position: TimeInterval)
+    func skip(stepInterval: TimeInterval)
+    
+    // TODO: Separate from `MediaPlayController`
+    func setupObservers(publisher: AnyPublisher<NowPlayingInfo?, Never>)
+}
+
+public struct Progress {
+    public let id: Int
+    public let progress: TimeInterval
+    public let duration: TimeInterval
+}
