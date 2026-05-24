651ddc0c215efd3d302a46e50daec0e53dbfeb20
GitHub Copilot <noreply@github.com>
2026-05-24 12:25:31 +1000

    Migrate to AGP 9.0 KMP plugin
    Replace com.android.library with com.android.kotlin.multiplatform.library.
    Move android config into kotlin { android { } } block.
    Move kotlinx-coroutines-core to commonMain dependencies.
    Disable AGP built-in Kotlin (incompatible with KGP 2.4.0-RC).

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
 buildscript {
-    dependencies.constraints {
-        "classpath"("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0-titan-218!!")
-    }
+
 }
 
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    alias(libs.plugins.androidLibrary).apply(false)
+    alias(libs.plugins.androidKmpLibrary).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
 }
